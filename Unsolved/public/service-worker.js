b975d50ef71b0fe26725927085a6b585e0b36dfb
Seth Glenn <Sbgees@yahoo.com>
2021-02-01 21:14:03 -0600

    built installation method inside service worker file

--- Unsolved/public/service-worker.js
+++ Unsolved/public/service-worker.js
@@ -7,5 +7,17 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 // installation
 self.addEventListener("install", function(evt) {
-    
-})
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
