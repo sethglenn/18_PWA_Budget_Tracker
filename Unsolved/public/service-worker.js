e2c79305627b635a30af8c7bd1778d1bda979155
Seth Glenn <Sbgees@yahoo.com>
2021-02-01 21:23:01 -0600

    finished service worker file for inital development

--- Unsolved/public/service-worker.js
+++ Unsolved/public/service-worker.js
@@ -43,3 +43,11 @@ self.addEventListener("fetch", evt => {
     return;
 
 })
+
+evt.respondWith(
+    caches.open(CACHE_NAME).then( cache => {
+        return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+        });
+    })
+)
