8d0dc828477f42b03bcb1e2cbe0a0eb928f9ea0e
Seth Glenn <Sbgees@yahoo.com>
2021-02-01 21:20:03 -0600

    finished fetching cache data

--- Unsolved/public/service-worker.js
+++ Unsolved/public/service-worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // installation
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
             return Promise.all(
@@ -23,18 +23,23 @@ self.addEventListener("install", function(evt) {
 });
 // fetching cache data
 self.addEventListener("fetch", evt => {
-    if(evt.request.url.includes("/api/")) {
+    if (evt.request.url.includes("/api/")) {
         console.log("[Service Worker]", fetch(data), evt.request.url);
     }
     evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
             return fetch(evt.request).then(response => {
-                if(response.status === 200) {
-                    
+                if (response.status === 200) {
+                    cache.put(evt.request.url, response.clone());
                 }
+                return response;
             })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
         })
-    )
 
+    )
+    return;
 
 })
