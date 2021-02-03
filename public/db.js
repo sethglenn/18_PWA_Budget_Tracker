fac51980046d8d93c0352f98744bc0d80413fbfe
Seth Glenn <Sbgees@yahoo.com>
2021-02-03 17:15:52 -0600

    created onupgrade method and on success method

--- public/db.js
+++ public/db.js
@@ -2,5 +2,16 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-    
-}
+    const db = event.target.results;
+    db.createObjectStore("pending", { autoIncrement: true});
+};
+
+request.onsuccess = function (event) {
+    db = event.target.request;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+
