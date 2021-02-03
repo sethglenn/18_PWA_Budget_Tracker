afa8360bb19edee73b128e6257f5edd5a38896b0
Seth Glenn <Sbgees@yahoo.com>
2021-02-03 17:26:22 -0600

    began constructing checkDatabase function for api calling offline

--- public/db.js
+++ public/db.js
@@ -24,3 +24,23 @@ function saveRecord(record) {
 
     store.add(record);
 }
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending", "readwrite"]);
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then()
+        }
+    }
+}
