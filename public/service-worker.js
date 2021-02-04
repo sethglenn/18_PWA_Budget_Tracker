d8d79499fc0e154c31a890bb0347cac4dfede1ae
Seth Glenn <Sbgees@yahoo.com>
2021-02-03 20:08:23 -0600

    tracked down typo error when passing *evt* to function

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // installation
-self.addEventListener("install", function(etv) {
+self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log("Files pre-cached suscessfully!");
@@ -37,29 +37,30 @@ self.addEventListener("install", function (evt) {
 self.addEventListener("fetch", evt => {
     if (evt.request.url.includes("/api/")) {
         console.log("[Service Worker]", fetch(data), evt.request.url);
-    }
-    evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-            return fetch(evt.request).then(response => {
-                if (response.status === 200) {
-                    cache.put(evt.request.url, response.clone());
-                }
-                return response;
-            })
-                .catch(err => {
-                    return cache.match(evt.request);
-                });
-        })
 
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request).then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                    .catch(err => {
+                        return cache.match(evt.request);
+                    });
+            })
+
+        )
+        return;
+    }
+
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
     )
-    return;
 
 })
-
-evt.respondWith(
-    caches.open(CACHE_NAME).then( cache => {
-        return cache.match(evt.request).then(response => {
-            return response || fetch(evt.request);
-        });
-    })
-)
