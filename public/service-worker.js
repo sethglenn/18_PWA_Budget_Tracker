7ea114185353ae668a1b7e03fb2e27a782b683df
Seth Glenn <Sbgees@yahoo.com>
2021-02-04 22:31:27 -0600

    fixed fetching error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
-const FILES_TO_CACHE = ['/', '/index.html', 'index.js', '/db.js', '/styles.css'];
-
 const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
+
+const FILES_TO_CACHE = ['/', '/index.html', 'index.js', '/db.js', '/styles.css', "/icons/icon-192x192.png", "/icons/icon-512x512.png"];
 
 // install
 self.addEventListener('install', function (evt) {
