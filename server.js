cdeaea78f4b3ac1edeccd32408c28a4b774ed3de
Seth Glenn <Sbgees@yahoo.com>
2021-02-04 22:51:16 -0600

    trying to resolve heroku issues

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(PORT || 3000, () => {
   console.log(`App running on port ${PORT}!`);
 });
