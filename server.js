a209ee7c2b4f81401338c03d9e7f6251f751c0d9
Seth Glenn <Sbgees@yahoo.com>
2021-02-04 22:56:08 -0600

    fixing heroku errors

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT || 3000, () => {
+app.listen(PORT || process.env.PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
